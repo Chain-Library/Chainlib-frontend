e322c589f72ec0f7fbf2c729c95f3694f43e637a
Ayomide Adeniran <ayomideratata@gmail.com>
2025-04-26 03:56:14 +0100

    Implement The About Page #10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-
-
+import AnalyticsChart from "@/components/author/AnalyticsChart";
+import About from "@/app/about-us/page";
 export default function Home() {
   return (
     <div>
       <h1>ChainLib Platform</h1>
+      {/* <AnalyticsChart /> */}
+      <About />
     </div>
   );
 }
