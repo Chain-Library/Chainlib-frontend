ba9a748c2f219be37c36bb8cd9573e56dd6c3d12
Kingsuite <Thedavidajayi@gmail.com>
2025-04-28 03:48:39 +0100

    created landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-
+import LandingPage from "@/app/landing-page/page"
 
 export default function Home() {
   return (
-    <div>
-      <h1>ChainLib Platform</h1>
-    </div>
+    <>
+      <div className="bg-white">
+       <LandingPage />
+      </div>
+    </>
   );
 }
