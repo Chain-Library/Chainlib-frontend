ba7a35b1ec7a278b7540ce09841e37bace5cf756
10daer <afolabireedwan@gmail.com>
2025-05-01 11:24:55 +0100

    adjusted for approval

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {},
+  experimental: {
+    turbo: {
+      enabled: true, // Set turbo to true within an object
+    },
+  },
 };
 
 export default nextConfig;
